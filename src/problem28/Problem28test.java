d09eb76812f30e0863a78456370044bf1757a646
{iamycx} <{956428086@qq.com}>
2017-10-16 10:44:00 +0800

    [update]

--- src/problem28/Problem28test.java
+++ src/problem28/Problem28test.java
@@ -6,8 +6,35 @@ package problem28;
 public class Problem28test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String str = "abc";
+		permutation(str);
+	}
 
+	private static void permutation(String str) {
+		if(str==null)
+			return;
+		char[] ch = str.toCharArray();
+		int p=0;
+		System.out.println(ch);
+		
+		char temp1 = ch[p];
+		ch[p] = ch[++p];
+		ch[p] = temp1;
+		while(!String.valueOf(ch).equals(str)){
+			System.out.println(ch);
+			if(p==ch.length-1){
+				char temp2 = ch[p];
+				ch[p] = ch[0];
+				ch[0] = temp2;
+				p=0;
+			}else{
+				char temp2 = ch[p];
+				ch[p] = ch[++p];
+				ch[p] = temp2;
+			}
+
+		}
+		
 	}
 
 }
